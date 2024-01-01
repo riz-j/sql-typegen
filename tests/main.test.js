9646a9b72b9cd5c3afa931362bbab73a8cadfc75
riz-j <adiputra979@gmail.com>
2024-01-01 16:00:40 +1100

    Add test case and modify existing ones accordingly

--- tests/main.test.js
+++ tests/main.test.js
@@ -5,14 +5,24 @@ test("Generate interface", done => {
     exec("bun src/main.ts  \
         --database postgresql://admin:newpassword@localhost:5433/postgres  \
         --table countries", (error, stdout, stderr) => {
-        expect(stdout).toContain("generated successfully");
+        expect(stdout).toContain("SUCCESS: country.ts has been generated");
+        done();
+    });
+});
+
+test("Produce error message when table is not found", done => {
+    exec("bun src/main.ts  \
+        --database postgresql://admin:newpassword@localhost:5433/postgres  \
+        --table nosuchtable", (error, stdout, stderr) => {
+        expect(stdout).toContain("ERROR: Table with name 'nosuchtable' is not found");
         done();
     });
 });
 
 test("Run build", done => {
     exec("./build.sh && ./run-build.sh", (error, stdout, stderr) => {
-        expect(stdout).toContain("generated successfully");
+        expect(stdout).toContain("SUCCESS");
+        expect(stdout).toContain("has been generated")
         done();
     });
 });
