3b45e7beb31c7b77fb95293e4cc640ff99d6bcbb
riz-j <adiputra979@gmail.com>
2024-01-04 19:31:51 +1100

    Generate appropriate error messages throught the function

--- src/functions/db.ts
+++ src/functions/db.ts
@@ -1,23 +1,47 @@
 import { PgDbOptions } from "@/models/db";
 import * as E from "fp-ts/Either";
 
-export const parse_db_connection_url = (db_connection_url: string): E.Either<Error, PgDbOptions>  => {
+/**
+ * Parses the given database connection URL and returns PostgreSQL database options.
+ * @param db_connection_url - The full database connection URL as a string.
+ * @returns Either an Error object, or an object of PgDbOptions.
+ */
+export const parse_db_connection_url = (db_connection_url: string): E.Either<Error, PgDbOptions> => {
     try {
         const url = new URL(db_connection_url);
-        
+
         if (url.protocol !== "postgresql:") {
-            return E.left(new Error("Only 'postgresql' is currently supported.\n\nMake sure you prefix the URL with postgresql://<your-connection-string>. Support for other databases coming soon."))
-        }        
+            return E.left(new Error("Invalid protocol. Only 'postgresql' is currently supported. Make sure you prefix the URL with postgresql://<your-connection-string>."));
+        }
 
         const host = url.hostname;
+        if (!host) {
+            return E.left(new Error("Invalid or missing host in the --database connection string."));
+        }
+
         const port = parseInt(url.port);
+        if (host === "localhost" && url.port && (isNaN(port) || port < 1 || port > 65535)) {
+            return E.left(new Error("Invalid or missing port in the --database connection string."));
+        }
+
         const database = url.pathname.split("/")[1];
+        if (!database) {
+            return E.left(new Error("Invalid or missing database name in the --database connection string."));
+        }
+
         const username = url.username;
+        if (!username) {
+            return E.left(new Error("Invalid or missing username in the --database connection string."));
+        }
+
         const password = url.password;
+        if (!password) {
+            return E.left(new Error("Invalid or missing password in the --database connection string."));
+        }
 
         return E.right({ host, port, database, username, password });
 
     } catch (error) {
-        return E.left(new Error(JSON.stringify(error)));
+        return E.left(new Error(`Error parsing database connection URL: ${error}`));
     }
 }
